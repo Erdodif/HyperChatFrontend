8546c4280d4f2f251bdfa55f1ff66ce17539f5d2
erdodif <erdodif@gmail.com>
2023-05-31 02:01:33 +0200

    formatting

--- src/lib/components/theme/ColorPicker.svelte
+++ src/lib/components/theme/ColorPicker.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+	import { onMount } from "svelte";
 
 	export let key;
 	export let value;
 	export let label;
 
-	let set:(key:string,value:string)=>void = (key:string,value:string)=>{};
-	
-	onMount(()=>{
-		set = (key:string, value:string) => document.documentElement.style.setProperty(`--${key}`,value);
+	let set: (key: string, value: string) => void = (
+		key: string,
+		value: string
+	) => {};
+
+	onMount(() => {
+		set = (key: string, value: string) => document.documentElement.style.setProperty(`--${key}`, value);
 	});
 
-	$: set(key,value);
+	$: set(key, value);
 </script>
 
 <label>
-	<input type="color" bind:value/> 
+	<input type="color" bind:value />
 	{label}
 </label>
 
 <style lang="scss">
-    input{
-        padding:0;
-        background-color: transparent;
-        border:transparent;
-    }
+	input {
+		padding: 0;
+		background-color: transparent;
+		border: transparent;
+	}
 </style>
