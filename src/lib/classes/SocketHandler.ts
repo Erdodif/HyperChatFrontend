7324994c66e3143c1ea4fb6f0e819215a9068b52
erdodif <erdodif@gmail.com>
2023-06-06 20:28:24 +0200

    Introducing SocketHandler

---
+++ src/lib/classes/SocketHandler.ts
@@ -0,0 +1,68 @@
+import { PUBLIC_SOCKET_URL } from "$env/static/public";
+
+export class EventHandler {
+    type: string;
+    callback: (event: any) => void;
+
+    constructor(type:string, callback:(event:any)=>void){
+        this.type = type;
+        this.callback = callback;
+    }
+}
+
+export default class SocketHandler {
+    #token: string;
+    #gateway: WebSocket;
+    #handlers: Map<string, (event: any) => void>;
+
+    constructor(token, handlerBundle: EventHandler[]) {
+        this.#token = token;
+        this.#handlers = new Map<string, (event: any) => void>();
+        this.attachHandlerbundle(handlerBundle);
+        this.#gateway = new WebSocket(PUBLIC_SOCKET_URL);
+        this.#gateway.onopen = this.openEvent;
+        this.#gateway.onclose = this.closeEvent;
+        this.#gateway.onmessage = this.messageEvent;
+    }
+
+    changeToken(token: string) {
+        this.#token = token;
+    }
+
+    addHandler(handler: EventHandler) {
+        this.#handlers.set(handler.type, handler.callback);
+    }
+
+    attachHandlerbundle(handlerBundle: EventHandler[]) {
+        for (const handler of handlerBundle) {
+            this.addHandler(handler);
+        }
+    }
+
+    readonly closeEvent = async (ec) => {
+        console.log("Disconnected");
+        console.warn(JSON.parse(ec.reason).data);
+    }
+
+    readonly openEvent = async () => {
+        console.debug(`Connecting to ${PUBLIC_SOCKET_URL}`);
+        this.#gateway.send(
+            JSON.stringify({ event: "IDENTIFY", data: { token: this.#token } })
+        );
+    }
+
+    readonly send: ((data: string | ArrayBufferLike | Blob | ArrayBufferView) => void) =
+        (data: string | ArrayBufferLike | Blob | ArrayBufferView) => {
+            this.#gateway.send(data);
+        }
+
+    readonly messageEvent = async (event:MessageEvent) => {
+        let payload = JSON.parse(event.data);
+        let fun = this.#handlers.get(payload.event);
+        if (fun == undefined) {
+            console.warn(`Unhandled event type ${payload.event}:`, payload);
+            return;
+        }
+        fun(payload.data);
+    }
+}
--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -1,157 +1,52 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { PUBLIC_SERVER_URL } from "$env/static/public";
-    import { PUBLIC_SOCKET_URL } from "$env/static/public";
-    import { onMount } from "svelte";
-    import {
-        Message,
-        SystemMessage,
-        ChatMessage,
-        UnsentMessage,
-    } from "../classes/Message";
-    import User from "../classes/User";
-    import { readonly } from "svelte/store";
     import ChatLog from "$lib/classes/ChatLog";
+    import SocketHandler, { EventHandler } from "$lib/classes/SocketHandler";
+    import { onMount } from "svelte";
+    import { user } from "$lib/stores/auth";
+    import User from "$lib/classes/User";
+    import Guild from "$lib/classes/Guild";
+    import { guilds } from "$lib/stores/guilds";
 
+    export let token: string;
     let chatlog: ChatLog = new ChatLog();
-    let token: string;
     let message: string;
-    export let user;
+
+    let socketHandler: SocketHandler;
+
+    let handlerBundle = [
+        new EventHandler("READY", (event) => {
+            $user = User.fromJson(event.user);
+            for (const guildData of event.guilds) {
+                $guilds.push(new Guild(guildData.id, guildData.name, guildData.owner_id));
+            }
+        }),
+        new EventHandler("GUILD_CREATE", (event) => {
+            $guilds[event.guild.id] = new Guild(event.guild.id, event.guild.name, event.guild.owner_id);
+            $guilds[event.guild.id].channels = event.guild.channels;
+            $guilds[event.guild.id].members = event.guild.members;
+        }),
+    ];
 
     onMount(() => {
-        token = localStorage.getItem("auth-token");
-        if (!token) goto("/login");
-        let gateway = new WebSocket(PUBLIC_SOCKET_URL);
-        gateway.addEventListener("open", async () => {
-            let now = Date.now();
-            console.log(`Connecting to ${PUBLIC_SOCKET_URL}`);
-            gateway.send(
-                JSON.stringify({ event: "IDENTIFY", data: { token: token } })
-            );
-        });
-        gateway.addEventListener("message", (event) => {
-            let content = JSON.parse(event.data);
-            switch (content.event) {
-                case "READY":
-                    handleReady(content.data);
-                    break;
-                case "MESSAGE_CREATE":
-                    handleMessage(content.data);
-                    break;
-                case "MEMBER_JOIN":
-                    handleJoin(content.data);
-                    break;
-                case "MEMBER_LEAVE":
-                    handleLeave(content.data);
-                    break;
-                case "INVALID_SESSION":
-                    handleInvalid(content.data);
-                    break;
-                default:
-                    console.warn("server is not talkin to me 💢");
-            }
-        });
-
-        gateway.addEventListener("close", (ec: CloseEvent) => {
-            console.log("Disconnected");
-            console.warn(JSON.parse(ec.reason).data);
-        });
+        socketHandler = new SocketHandler(token, handlerBundle);
     });
-
-    function handleReady(ready: any) {
-        chatlog.push(new SystemMessage("You just dropped in, tenno."));
-        chatlog = chatlog;
-        console.log("Connected");
-        localStorage.setItem("user", ready);
-    }
-
-    function handleMessage(message: any) {
-        chatlog.push(
-            new ChatMessage(
-                new User(
-                    message.author.id,
-                    message.author.user_name,
-                    message.author.display_name
-                ),
-                message.content,
-                message.id
-            )
-        );
-        chatlog = chatlog;
-    }
-
-    function handleJoin(join: any) {
-        if ((join.id as string) == (user.id as string)) return;
-        chatlog.push(
-            new SystemMessage(`${join.display_name} is now with you, punk!`)
-        );
-        chatlog = chatlog;
-    }
-
-    function handleLeave(leave: any) {
-        chatlog.push(
-            new SystemMessage(`${leave.display_name} just left this realm...`)
-        );
-        chatlog = chatlog;
-    }
-
-    function handleInvalid(invalid: any) {
-        goto("/login");
-    }
-
-    async function sendMessage(message: string) {
-        let nonce = `${Date.now()}-\"${message}\"`;
-        const response = fetch(PUBLIC_SERVER_URL + "/message/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: token,
-            },
-            body: JSON.stringify({
-                content: message,
-                nonce: nonce,
-            }),
-        });
-        let index = chatlog.push(new UnsentMessage(user, message, nonce));
-        const content = await (await response).json();
-        if (content.nonce == nonce) {
-            chatlog.makeSent(index, content.id);
-            chatlog = chatlog;
-        } else {
-            console.error(content);
-        }
-    }
-
-    function getAuthor(message: Message): string {
-        if (message instanceof SystemMessage) {
-            return "Sys";
-        }
-        if (
-            message instanceof UnsentMessage &&
-            (message as UnsentMessage).author === user
-        ) {
-            return "You";
-        } else {
-            return (message as ChatMessage).author.display_name;
-        }
-    }
 </script>
 
 <div class="chat-holder">
     <div class="le-chat">
         <div class="scrollable">
-            <div class="content">
+            <!--<div class="content">
                 {#each chatlog.values as msg}
                     <span class="message">{getAuthor(msg)} - {msg.content}</span
                     >
                 {/each}
-            </div>
+            </div>-->
         </div>
     </div>
-    <form on:submit|preventDefault={() => sendMessage(message)}>
+    <!--<form on:submit|preventDefault={() => sendMessage(message)}>
         <input type="text" bind:value={message} />
         <button type="submit">Send ship</button>
-    </form>
+    </form>-->
 </div>
 
 <style lang="scss">
