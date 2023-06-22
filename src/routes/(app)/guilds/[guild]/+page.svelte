3072b357b9225a9e524437f7a890935d4bb2515f
erdodif <erdodif@gmail.com>
2023-06-23 00:52:40 +0200

    Singular guild page

---
+++ src/routes/(app)/guilds/[guild]/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let data;
+    let guild = data.guild;
+
+    $: guild = data.guild;
+</script>
+
+{#if guild !== undefined}
+    <h1>{guild.name}</h1>
+
+    <h2>Members</h2>
+
+    <div>
+        {#each guild.members as user, index}
+            <div>
+                <span>#{index + 1}</span>
+                <span>{user.displayName}</span>
+            </div>
+        {/each}
+    </div>
+
+    <h2>Channels</h2>
+
+    <div>
+        {#each guild.channels as channel}
+            <div>
+                <span>
+                    {channel.name}
+                </span>
+            </div>
+        {/each}
+    </div>
+{/if}
