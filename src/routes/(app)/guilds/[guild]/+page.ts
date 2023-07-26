b9fa75037c705ef66a743444378bf3950bbe84cc
erdodif <erdodif@gmail.com>
2023-07-27 00:53:59 +0200

    Getting used to regular subscribtion method

--- src/routes/(app)/guilds/[guild]/+page.ts
+++ src/routes/(app)/guilds/[guild]/+page.ts
@@ -5,9 +5,11 @@ import { error } from "@sveltejs/kit"
 
 export function load({ params }) {
     //return derived<Guild>(guilds,);
-    let guild = guildSet.value.guilds.get(params.guild);
+    let guild:Guild;
+    let unsubscribe = guildSet.subscribe((guildSet)=>guild = guildSet.guilds.get(params.guild));
     if (guild) {
         return { guild };
     }
+    unsubscribe();
     throw error(404,'Guild not found');
 }
