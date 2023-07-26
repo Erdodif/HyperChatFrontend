b0a5912b6e92f7f6c9b1ce46a4570f0c8657f6eb
erdodif <erdodif@gmail.com>
2023-07-27 00:55:47 +0200

    Optimizing imports, adding chatlog object

--- src/routes/(app)/channels/[channel]/+page.svelte
+++ src/routes/(app)/channels/[channel]/+page.svelte
@@ -1,35 +1,24 @@
 <script lang="ts">
     import GuildPage from "$lib/components/GuildPage.svelte";
-    import { goto } from "$app/navigation";
-    import { PUBLIC_SERVER_URL } from "$env/static/public";
-    import { token } from "$lib/stores/auth";
     import { guildSet } from "$lib/stores/guildSet";
     import { page } from "$app/stores";
     import type Guild from "$lib/classes/Guild";
-    import type {Channel} from "$lib/classes/Guild";
+    import type Channel from "$lib/classes/Channel";
+    import ChatLog from "$lib/components/ChatLog.svelte";
+    import { readable } from "svelte/store";
 
-    let channel: Channel = $guildSet.searchChannel($page.params.channel)!;
-    //let guild = channel.guild;
-    $: {
-        channel = $guildSet.searchChannel($page.params.channel)!;
-    //let guild = channel.guild;
-    }
 </script>
 
-{#if channel == null}
-    {@debug $page}
-{:else}
-    {console.log(channel)}
-<GuildPage guild={channel.guild}>
+{#if $guildSet.searchChannel($page.params.channel)}
+<GuildPage guild={$guildSet.searchChannel($page.params.channel).guild}>
     <div class="channel">
         <div class="head">
             <h1>
-                {channel.name}
+                {$guildSet.searchChannel($page.params.channel).name}
             </h1>
         </div>
-        <section>
-
-        </section>
+        {console.log($guildSet.searchChannel($page.params.channel).chat)}
+        <ChatLog chatLog={$guildSet.searchChannel($page.params.channel).chat}/>
     </div>
     <!--
     <h1>{guild.name}</h1>
