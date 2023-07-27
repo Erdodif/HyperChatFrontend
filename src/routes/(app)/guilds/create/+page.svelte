193c5e98c158d5cf1830d1bafbfb03451e98fb11
erdodif <erdodif@gmail.com>
2023-07-27 16:24:28 +0200

    Join guild by id

--- src/routes/(app)/guilds/create/+page.svelte
+++ src/routes/(app)/guilds/create/+page.svelte
@@ -1,35 +1,45 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import { PUBLIC_SERVER_URL } from "$env/static/public";
-    import { token } from "$lib/stores/auth";
+    import Rest, { RestMethod } from "$lib/classes/Rest";
     let name: string = "";
+    let id :string = "";
 
-    const handleSubmit = async () => {
-        const response = await fetch(PUBLIC_SERVER_URL + "/guilds", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": $token
-            },
-            body: JSON.stringify({name:name}),
-        });
+    const createGuild = async () => {
+        const response = await Rest.sendToServer("guilds",{name:name});
         let res = await response.json();
         if(response.ok){
             throw goto(`/guilds/${res.id}`);
         }
-        console.log(res);
+    };
+
+    const joinGuild = async () => {
+        const response = await Rest.getFromServer(`guilds/${id}/members`,RestMethod.POST);
+        if(response.ok){
+            let res = await response.json();
+            console.log(res);
+            throw goto(`/guilds/${res.id}`);
+        }
+        console.log(response);
     };
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
+<form on:submit|preventDefault={createGuild}>
+    <h1>Create new Guild</h1>
     <input type="text" bind:value={name} placeholder="Guild Name" />
-    <span>{name}</span>
     <button type="submit">Create</button>
+</form>
+<form on:submit|preventDefault={joinGuild}>
+    <h1>Or join by Id</h1>
+    <input type="text" bind:value={id} placeholder="Guild Id" />
+    <button type="submit">Join</button>
 </form>
 
 <style lang="scss">
     form {
         width: 100%;
-        height: 100%;
+        margin-block-end:2em;
+    }
+    h1{
+        font-size:1.5em;
     }
 </style>
