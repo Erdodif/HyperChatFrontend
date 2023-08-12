e1ffa0caa0b008ffc5f1af8b98d1e094fbc10a0b
erdodif <erdodif@gmail.com>
2023-08-13 01:54:18 +0200

    introducing attachment thumbnail based on mime

---
+++ src/lib/components/MimeImg.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    export let mime: string = "unknown";
+
+    const mimeSource = `${$page.url.origin}/mimetypes/${mime.replaceAll(
+        "/",
+        "-"
+    )}.svg`;
+</script>
+
+<img
+    class="mime"
+    src={mimeSource}
+    alt={mime}
+    on:error={() => (this.src = `${$page.url.origin}/mimetypes/unknown.svg`)}
+/>
+
+<style lang="scss">
+    .mime{
+        margin-inline: auto;
+        max-width: 5em;
+        max-height: 5em;
+    }
+</style>
